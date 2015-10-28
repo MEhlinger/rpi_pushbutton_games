827ad452288dd3b98486a1e39209ad364e27d3c8
Marshall Ehlinger <ehlingermd15@mounties.mansfield.edu>
2015-10-28 17:15:36 -0400

    Add pushbutton class and test pygame app

---
+++ pushbutton.py
@@ -0,0 +1,29 @@
+# GPIO Capactive Sensor Input Module
+# For Raspberry Pi
+# by Marshall Ehlinger, Oct 2015
+
+import time
+import RPi.GPIO as GPIO
+
+class PushButton:
+
+	def __init__(self, pinNum):
+		self.pinNumber = pinNum
+		self.lastPress = 0.00
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(self.pinNumber, GPIO.IN)
+
+	def isPressed(self):
+		return not GPIO.input(self.pinNumber) # Pushbutton defaults open
+
+	def getPinNumber(self):
+		return self.pinNumber
+
+	def getLastPress(self):
+		return self.lastPress
+
+	def setLastPressToNow(self):
+		self.lastPress = time.Clock()
+
+	def setLastPressAsArg(self, lastPressTime):
+		self.lastPress = lastPressTime 
